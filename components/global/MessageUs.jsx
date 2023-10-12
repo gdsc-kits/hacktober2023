a065cab1f42131373b5cf4b952c27ed53ce86724
gdsc-kits-admin <144369631+gdsc-kits-admin@users.noreply.github.com>
2023-10-12 11:08:20 +0000

    Modified profile cards

--- components/global/MessageUs.jsx
+++ components/global/MessageUs.jsx
@@ -1,22 +1,22 @@
-// 'use client';
-// import React, { useEffect } from 'react';
+'use client';
+import React, { useEffect } from 'react';
 
-// import { initMessageGator } from 'message-gator';
+import { initMessageGator } from 'message-gator';
 
-// function MessageUs() {
+function MessageUs() {
   
-//   useEffect(() => {
+  useEffect(() => {
 
-//     // Replace the userId with your own from https://mg.pushkaryadav.in
+    // Replace the userId with your own from https://mg.pushkaryadav.in
 
-//     initMessageGator({
-//       userId: `8d5ca0c4-1c9f-451b-9dbd-a824017ca9f1`,
-//       customCSS: '#cta-messsage-gator{background: #242b2e; color: #fff;border:none; border-radius: 25px; padding: 10px 20px; font-size: 16px; font-weight: 600; position: fixed; bottom: 20px; right: 20px; z-index: 9999; cursor: pointer;} #cta-messsage-gator:active{border:none;}',
-//       buttonText: 'Drop a Message!',
-//     });
-//   }, []);
+    initMessageGator({
+      userId: `8d5ca0c4-1c9f-451b-9dbd-a824017ca9f1`,
+      customCSS: '#cta-messsage-gator{background: #242b2e; color: #fff;border:none; border-radius: 25px; padding: 10px 20px; font-size: 16px; font-weight: 600; position: fixed; bottom: 20px; right: 20px; z-index: 9999; cursor: pointer;} #cta-messsage-gator:active{border:none;}',
+      buttonText: 'Drop a Message!',
+    });
+  }, []);
 
-//   return <></>;
-// }
+  return <></>;
+}
 
-// export default MessageUs;
+export default MessageUs;
